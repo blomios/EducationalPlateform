6179e146fbf4bbd6ef0715529af9c64312dee565
G_ <33670394+codng101@users.noreply.github.com>
2019-06-06 15:34:54 +0200

    ajout addStudent

---
+++ LO54_Projet/UVS/AddStudentUV.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Importation des élèves" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddStudentUV.aspx.cs" Inherits="LO54_Projet.UVS.AddStudentUV" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %></h2>
+    <p class="text-danger">
+        <asp:Literal runat="server" ID="ErrorMessage" />
+    </p>
+
+    <div class="form-horizontal">
+        <br />
+        <h3>Importer des élèves</h3>
+        
+        <div class="form-group">
+            <asp:FileUpload id="FileUploadControl" runat="server" />
+             <asp:Button runat="server" id="UploadButton" text="Upload" onclick="UploadButton_Click" />
+            <br /><br />
+            <asp:Label runat="server" id="StatusLabel" text="Upload status: " />
+            <br /><br />
+            <asp:Button ID="Button_Annuler" runat="server" BorderStyle="None" CssClass="btn active" Text="Annuler" OnClick="Button_Redirect" />
+        </div>
+    </div>
+</asp:Content>
---
+++ LO54_Projet/UVS/AddStudentUV.aspx.cs
@@ -0,0 +1,77 @@
+﻿using LO54_Projet.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace LO54_Projet.UVS
+{
+    public partial class AddStudentUV : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+
+        }
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
+                string prenom="";
+                string nom="";
+                string email;
+                string pwd;
+                try
+                {
+                    string filename = Path.GetFileName(FileUploadControl.FileName);
+                    FileUploadControl.SaveAs(Server.MapPath("~/") + filename);
+                    StatusLabel.Text = "Upload status: File uploaded!";
+                    string line;
+                    System.IO.StreamReader file =
+                    new System.IO.StreamReader(Server.MapPath("~/") + filename);
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        Regex regex = new Regex(@"(?<=;)(\w*)(?=;)");
+                        MatchCollection match = regex.Matches(line);
+                        if (match.Count != 2){
+                            continue;
+                        }
+                        int i = 0;
+                        foreach (Match m in match)
+                        {
+                            if (i == 0) { prenom = m.Value; }
+                            if (i == 1) { nom = m.Value; }
+                            i++;
+                        }
+                        Regex rx = new Regex(@"(?<=;)(\w*)$");
+                        Match mpwd = rx.Match(line);
+                        var user = new ApplicationUser(nom,prenom ) ;
+                        IdentityResult result = manager.Create(user,mpwd.Value );
+                        if (result.Succeeded)
+                        {
+                            
+                        }
+                        else
+                        {
+                            ErrorMessage.Text = result.Errors.FirstOrDefault();
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }   
+        }
+    }
+}
